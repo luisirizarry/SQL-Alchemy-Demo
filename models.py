a1ff1c8d9411c622a28e506bea5f595c5bac12e6
Luis Irizarry <lirizarry6178@gmail.com>
2024-09-23 23:42:12 -0400

    Created Pet object for pet model

--- models.py
+++ models.py
@@ -9,40 +9,21 @@ def connect_db(app):
 
 # MODELS GO BELOW!
 class Pet(db.Model):
+    """Pet."""
     __tablename__ = 'pets'
-
-    id = db.Column(db.Integer,
-                   primary_key=True,
-                   autoincrement=True)
-
-    name = db.Column(db.String(50),
-                     nullable=False,
-                     unique=True)
-
-    species = db.Column(db.String(30), nullable=True)
-
-    hunger = db.Column(db.Integer, nullable=False, default=20)
-
-    @classmethod
-    def get_by_species(cls, species):
-        return cls.query.filter_by(species=species).all()
-
-    @classmethod
-    def get_all_hungry(cls):
-        return cls.query.filter(Pet.hunger > 20).all()
 
     def __repr__(self):
         p = self
         return f"<Pet id={p.id} name={p.name} species={p.species} hunger={p.hunger}>"
 
-    def greet(self):
-        return f"I'm {self.name} the {self.species}"
+    id = db.Column(db.Integer, 
+                   primary_key=True, 
+                   autoincrement=True)
     
-    def feed(self, amt=20):
-        """Update hunger based off of amt """
-        self.hunger -= amt
-        self.hunger = max(self.hunger, 0)
+    name = db.Column(db.String(50), 
+                     nullable=False,
+                     unique=True)
+    
+    species = db.Column(db.String(30), nullable=False)
 
-                     
-
-
+    hunger = db.Column(db.Integer, default=20)
